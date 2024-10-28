bd15b47d3894e6b03c0e434f9ae2eeb36bef35c4
Niten Kumawat <nitenkumawat@gmail.com>
2024-10-28 23:21:03 +0530

    first commit

---
+++ script/script.js
@@ -0,0 +1,13 @@
+
+let hours=document.getElementById("hours");
+let mins=document.getElementById("minutes");
+let sec=document.getElementById("seconds");
+
+setInterval(()=>{
+    let currentTime = new Date();
+    hours.innerHTML= (currentTime.getHours()<10?"0":"")+currentTime.getHours();
+    mins.innerHTML= (currentTime.getMinutes()<10?"0":"")+currentTime.getMinutes();
+    sec.innerHTML= (currentTime.getSeconds()<10?"0":"")+currentTime.getSeconds();
+    
+},1000)
+
